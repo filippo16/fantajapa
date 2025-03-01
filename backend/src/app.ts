4fa082d04dc7a619ca8800198853cbde7b55c636
filippo16 <filippo.lutterotti2@gmail.com>
2025-03-01 17:50:08 +0100

    all ok

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -3,13 +3,17 @@ import cors from 'cors';
 import bodyParser from "body-parser";
 import dotenv from 'dotenv';
 
+import https from 'https';
+import fs from 'fs';
+import path from 'path';
+
 import sseRoutes from './routes/sse';
 import rot from './routes/router';
 
 dotenv.config();
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 443;
 
 // Middleware
 app.use(cors());
@@ -21,6 +25,17 @@ app.use(rot);
 
 app.use(express.static("dist-frontend"));
 
-app.listen(PORT, () => {
-    console.log(`Server in ascolto su http://localhost:${PORT}`);
+const sslOptions = {
+    key: fs.readFileSync(path.join(__dirname, 'miocert.key')),
+    cert: fs.readFileSync(path.join(__dirname, 'miocert.crt'))
+};
+  // /etc/ssl/private/miocert.key
+///etc/ssl/certs/miocert.crt
+// Avvio del server HTTPS
+https.createServer(sslOptions, app).listen(PORT, () => {
+console.log(`Server in ascolto su https://localhost:${PORT}`);
 });
+
+// app.listen(PORT, () => {
+//     console.log(`Server in ascolto su http://localhost:${PORT}`);
+// });
